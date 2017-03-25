858e01e3cdc718f1c3d58c3cf80410f96f3db595
sharon smith <sharon.e.smith2@gmail.com>
2017-03-25 11:41:55 -0500

    event handler for styling chgs of selected card done

--- main.js
+++ main.js
@@ -1,28 +1,4 @@
 
-var inventoryLoaded = false;
-
-// function populatePage (inventory) {
-
-// console.log("in populatePage / inventory :: ", inventory);
-
-//   // Loop over the inventory and populate the page
-  
-//   // Now that the DOM is loaded, establish all the event listeners needed
-//   // CarLot.activateEvents();
-// }
-
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
-
 CarLot.loadInventory();
-
-// do {
-// 	inventoryLoaded = CarLot.checkInventoryStatus();
-// 	console.log("inventoryNotLoaded :: ", inventoryLoaded);
-// } while (!inventoryLoaded); 
-
-// populatePage(CarLot.getCarInventory())
-
-// oldCarLot.populatePage(CarLot.getCarInventory())
 
 
--- output.js
+++ output.js
@@ -20,7 +20,7 @@ var inventoryDOM = document.getElementById("outputDOM");
 
 		for (var i=0; i<inventory.cars.length; i++) {
 
-			tableString += `<div id="i+1" class="carCard col-3">`;
+			tableString += `<div id=${i+1} class="carCard col-3">`;
 			thisCar = inventory.cars[i];
 
 			tableString += `<p class="carYear">${thisCar.year}</p>`;
@@ -47,14 +47,74 @@ var inventoryDOM = document.getElementById("outputDOM");
 
 		inventoryDOM.innerHTML = tableString;
 
+		oldCarLot.activateEvents();
 	}; // <populatePage>
 
 
+	// function stablishes all the event listeners for the application
+	//
+	// From the navbar: <Home>, <Add>, <Delete>, <Current Inventory>
+	//
+	// When the user clicks on a Car card
+	// * the borderWidth on the card changes to <thick>
+	// * the background color of the card changes to <#EBC0C0> (a grayish rose color)
+	// * the <carDesc> text displays in the <Car Description> text input field
+	// * and whatever text the user enters in the input field is bound to the 
+	// * <carDesc> info in the Car card
+	oldCarLot.activateEvents = function() {
+console.log("activating Events");
+	var homeLink = document.getElementById("homeHRef");
+	var addLink = document.getElementById("addHRef");
+	var deleteLink = document.getElementById("deleteHRef");
+	var inventoryLink = document.getElementById("inventoryHRef");
+	var carCards = document.getElementById("outputDOM");
+	var selectedCard = "null";
 
+		homeLink.addEventListener("click", function() {
+	    	alert("Home Screen is under construction ...");
+		});
 
-// Now that the DOM is loaded, establish all the event listeners needed
-// CarLot.activateEvents();
+		addLink.addEventListener("click", function() {
+	    	alert("Adding Cars to Inventory Feature is under construction ...");
+		});
 
+		deleteLink.addEventListener("click", function() {
+	    	alert("Deleting Cars from Inventory Feature is under construction ...");
+		});
+
+		inventoryLink.addEventListener("click", function() {
+	    	oldCarLot.populatePage(CarLot.getCarInventory());
+		});
+
+		carCards.addEventListener("click", function(e) {
+
+			// reset border of last selected car 
+			// to initial state
+			if (selectedCard !== "null") {
+				selectedCard.style.borderWidth="thin";
+				selectedCard.style.backgroundColor="#EBDFDF";
+			}
+
+			// isolate the selected target Car card
+			if (e.target.classList.contains("carCard") === true) {
+				selectedCard = e.target;
+
+			} else if 
+				((e.target.classList.contains("carYear") === true) || 
+				(e.target.classList.contains("makeModel") === true) || 
+				(e.target.classList.contains("carPrice") === true) || 
+				(e.target.classList.contains("carDesc") === true))
+				{
+				selectedCard = e.target.parentNode;
+			}
+
+			// change the border thickness and background color
+			// of the selected card
+			selectedCard.style.borderWidth="thick";
+			selectedCard.style.backgroundColor="#EBC0C0";
+		});
+
+	};
 
 	return oldCarLot;
 
